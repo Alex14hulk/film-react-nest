455d6ba8fc2fcc957035efc80b616eb86557c461
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2025-10-31 22:32:02 +0500

    one

--- backend/src/logger/json_logger.spec.ts
+++ backend/src/logger/json_logger.spec.ts
@@ -1,33 +1,73 @@
 import { JsonLogger } from './json_logger';
 
 describe('JsonLogger', () => {
-  it('logger.log() should print correct json', () => {
-    const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
-    const logger = new JsonLogger();
-    const logText = 'Any logging text';
-    logger.log(logText);
-    expect(logSpy).toHaveBeenCalledWith(
-      `{\"level\":\"log\",\"message\":\"${logText}\",\"optionalParams\":[[]]}`,
+  let jsonLoggerService: JsonLogger;
+
+  beforeEach(() => {
+    jsonLoggerService = new JsonLogger();
+  });
+
+  it('.log() should call log method of the service', () => {
+    const mockTextConsole = {
+      level: 'log',
+      message: 'test message',
+      optionalParams: [null],
+    };
+    jest
+      .spyOn(jsonLoggerService, 'log')
+      .mockImplementation((message, optionalParams) =>
+        jsonLoggerService.formatMessage('log', message, optionalParams),
+      );
+    expect(jsonLoggerService.log('test message')).toEqual(
+      JSON.stringify(mockTextConsole),
     );
   });
 
-  it('logger.warn() should print correct json', () => {
-    const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
-    const logger = new JsonLogger();
-    const warnText = 'Any warning text';
-    logger.warn(warnText);
-    expect(warnSpy).toHaveBeenCalledWith(
-      `{\"level\":\"warn\",\"message\":\"${warnText}\",\"optionalParams\":[[]]}`,
+  it('.error() should call error method of the service', () => {
+    const mockTextConsole = {
+      level: 'error',
+      message: 'test message',
+      optionalParams: [null],
+    };
+    jest
+      .spyOn(jsonLoggerService, 'error')
+      .mockImplementation((message, optionalParams) =>
+        jsonLoggerService.formatMessage('error', message, optionalParams),
+      );
+    expect(jsonLoggerService.error('test message')).toEqual(
+      JSON.stringify(mockTextConsole),
     );
   });
 
-  it('logger.error() should print correct json', () => {
-    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-    const logger = new JsonLogger();
-    const errorText = 'Any error text';
-    logger.error(errorText);
-    expect(errorSpy).toHaveBeenCalledWith(
-      `{\"level\":\"error\",\"message\":\"${errorText}\",\"optionalParams\":[[]]}`,
+  it('.debug() should call debug method of the service', () => {
+    const mockTextConsole = {
+      level: 'debug',
+      message: 'test message',
+      optionalParams: [null],
+    };
+    jest
+      .spyOn(jsonLoggerService, 'debug')
+      .mockImplementation((message, optionalParams) =>
+        jsonLoggerService.formatMessage('debug', message, optionalParams),
+      );
+    expect(jsonLoggerService.debug('test message')).toEqual(
+      JSON.stringify(mockTextConsole),
+    );
+  });
+
+  it('.fatal() should call fatal method of the service', () => {
+    const mockTextConsole = {
+      level: 'fatal',
+      message: 'test message',
+      optionalParams: [null],
+    };
+    jest
+      .spyOn(jsonLoggerService, 'fatal')
+      .mockImplementation((message, optionalParams) =>
+        jsonLoggerService.formatMessage('fatal', message, optionalParams),
+      );
+    expect(jsonLoggerService.fatal('test message')).toEqual(
+      JSON.stringify(mockTextConsole),
     );
   });
 });
